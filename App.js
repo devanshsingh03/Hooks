d0f74f9d6e4e41cc5e2d6eb4f4f98f2745d92e46
Devansh Singh <114738136+devanshsingh03@users.noreply.github.com>
2024-12-31 05:41:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Resturant1 from './component/Basics/Resturant1'
+//import UseState from './component/Hooks/useState'
+import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+ return (
+  <UseReducer />
+    
+  )
+}
+
+
+export default App;
